feadc060774589c5d9cc21983f6bbcdefc1a5630
GregoryMaitre <gregory.maitre@epfl.ch>
2014-02-22 18:19:48 +0100

    Start GUI

--- BordComputer2014/src/GUI/GUI.java
+++ BordComputer2014/src/GUI/GUI.java
@@ -1,4 +1,10 @@
 package GUI;
+
+import java.awt.Dimension;
+import java.awt.Toolkit;
+
+import javax.swing.JFrame;
+
 /**
  * TODO: Comment this class
  * 
@@ -6,5 +12,19 @@ package GUI;
  * 
  */
 public class GUI {
+	private JFrame frame;
 
+	private GUI() {
+		frame = new JFrame();
+		frame.setTitle("Voiture");
+		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+		frame.setBounds(0, 0, (int) (screenSize.getWidth() / 4.0),
+				(int) (screenSize.getHeight() / 3.0));
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+	}
+
+	public static void main(String[] args) {
+		new GUI();
+	}
 }
