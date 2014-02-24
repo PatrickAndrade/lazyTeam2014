0256f1cd81ec59b76ad2c0527dc0c752880a7309
GregoryMaitre <gregory.maitre@epfl.ch>
2014-02-24 20:17:26 +0100

    Refresh 1sec

--- BordComputer2014/src/Sensor/Sensors.java
+++ BordComputer2014/src/Sensor/Sensors.java
@@ -19,7 +19,7 @@ public class Sensors implements Runnable {
 
     private void waitOneSecond() {
         try {
-            Thread.sleep(100);
+            Thread.sleep(1000);
         } catch (InterruptedException e) {
         }
     }
